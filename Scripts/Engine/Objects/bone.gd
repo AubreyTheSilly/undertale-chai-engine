223c34957fe5367f63577c0ffcd0c326e2fc871b
DarkMinerULTRA <henry_burton@outlook.com>
2026-04-22 21:36:25 +0930

    bone engine fix wip
    normal bones are done but doublebones.... yeesh....

--- Scripts/Engine/Objects/bone.gd
+++ Scripts/Engine/Objects/bone.gd
@@ -3,24 +3,33 @@ extends Node2D
 
 @export_enum("White","Blue","Orange","Green") var attack_type : String = "White"
 @export var damage := 1
-@export_range(0,100,1,"or_greater") var height : float = 0
+@export_range(10,100,1,"or_greater") var height : float = 0
 var velocity : Vector2 = Vector2.ZERO
 var rotation_velocity : float = 0
 @export var pap : bool = false
 
 func _draw():
+	# old code
+	#if pap:
+		#draw_texture_rect(preload("res://Sprites/Battle/Attacks/bone_pap.png"),Rect2(Vector2(-2,-5-height),Vector2(5,5)),false)
+		#draw_rect(Rect2(Vector2(0,-height),Vector2(2.5,height)),Color.WHITE)
+	#else:
+		#draw_texture_rect(preload("res://Sprites/Battle/Attacks/bone.png"),Rect2(Vector2(-2.5,-5-height),Vector2(5,5)),false)
+		#draw_rect(Rect2(Vector2(-1,-height),Vector2(4,height)),Color.WHITE)
 	if pap:
-		draw_texture_rect(preload("res://Sprites/Battle/Attacks/bone_pap.png"),Rect2(Vector2(-2.5,-5-height),Vector2(6.5,5)),false)
-		draw_rect(Rect2(Vector2(-0.5,-height),Vector2(2.5,height)),Color.WHITE)
+		draw_texture_rect(preload("res://Sprites/Battle/Attacks/bone_pap.png"),Rect2(Vector2(-2.5,-5),Vector2(5,-5)),false)
+		draw_texture_rect(preload("res://Sprites/Battle/Attacks/bone_pap.png"),Rect2(Vector2(-2.5,-height),Vector2(5,5)),false)
+		draw_rect(Rect2(Vector2(-1,-height+5),Vector2(2,height-10)),Color.WHITE)
 	else:
-		draw_texture_rect(preload("res://Sprites/Battle/Attacks/bone.png"),Rect2(Vector2(-2.5,-5-height),Vector2(6.5,5)),false)
-		draw_rect(Rect2(Vector2(-1.25,-height),Vector2(4,height)),Color.WHITE)
+		draw_texture_rect(preload("res://Sprites/Battle/Attacks/bone.png"),Rect2(Vector2(-2.5,-5),Vector2(5,-5)),false)
+		draw_texture_rect(preload("res://Sprites/Battle/Attacks/bone.png"),Rect2(Vector2(-2.5,-height),Vector2(5,5)),false)
+		draw_rect(Rect2(Vector2(-1.5,-height+5),Vector2(3,height-10)),Color.WHITE)
 
 func _process(_delta):
 	queue_redraw()
 	
-	$attack/CollisionShape2D.shape.size.x = 4-(1.5*int(pap))
-	$attack/CollisionShape2D.shape.size.y = 5+height
+	$attack/CollisionShape2D.shape.size.x = 3-(1*int(pap))
+	$attack/CollisionShape2D.shape.size.y = height
 	$attack/CollisionShape2D.position.y = -($attack/CollisionShape2D.shape.size.y/2)
 	
 	position += velocity
